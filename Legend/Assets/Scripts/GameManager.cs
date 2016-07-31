87f3a78e212ee5bff123781a17e30f2b4a18dbe7
Ryan Alameddine <rhalameddine@gmail.com>
2016-07-31 10:51:22 -0700

    Added Menu and began adding Player

---
+++ Legend/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    static GameManager instance;
+
+    public static GameManager Instance { get { return instance; } }
+
+    public string UserName;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
