aebc994a808758f1f6bafae0be8382a038861c37
Ryan Alameddine <rhalameddine@gmail.com>
2016-08-07 10:57:55 -0700

    Added Player

---
+++ Legend/Assets/Layering.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Layering : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update () {
+        spriteRenderer.sortingOrder = (int)(-transform.position.y * 10);
+	}
+}
--- Legend/Assets/Scripts/PlayerController.cs
+++ Legend/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerController : MonoBehaviour {
-
-	// Use this for initialization
+    Animator animator;
+    Rigidbody2D rigidbody2d;
+    [SerializeField]
+    float Speed = 1;
+    Vector2 direction;
+    bool moved;
 	void Start () {
-	
+        animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate () {
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+            moved = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+            moved = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+            moved = true;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+            moved = true;
+        }
+        if (!moved)
+        {
+            direction += Vector2.zero;
+        }
+        animator.SetBool("Up", Input.GetKey(KeyCode.W));
+        animator.SetBool("Down", Input.GetKey(KeyCode.S));
+        animator.SetBool("Left", Input.GetKey(KeyCode.A));
+        animator.SetBool("Right", Input.GetKey(KeyCode.D));
+        rigidbody2d.AddForce(direction * Speed);
+        moved = false;
+    }
 }
