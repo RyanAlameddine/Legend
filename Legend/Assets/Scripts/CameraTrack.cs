767ab74ac72d3b11830b2fdbfe67d91dcc71ca60
Ryan Alameddine <rhalameddine@gmail.com>
2016-09-25 10:46:56 -0700

    Added Camera clamp, background, and level border

---
+++ Legend/Assets/Scripts/CameraTrack.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CameraTrack : MonoBehaviour {
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Level level;
+
+    public bool TrackRotation;
+    public bool TrackPosition;
+
+    [SerializeField]
+    Vector3 offset;
+    [SerializeField]
+    Vector2 clampoffset;
+
+    Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+	
+	void Update () {
+        if (TrackPosition)
+        {
+            transform.position = target.position + offset;
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, level.Center.x - level.Width/2 + camera.orthographicSize * camera.aspect + clampoffset.x, level.Center.x + level.Width / 2 - camera.orthographicSize * camera.aspect - clampoffset.x), 
+                                            Mathf.Clamp(transform.position.y, level.Center.y - level.Height / 2 + camera.orthographicSize + clampoffset.y, level.Center.y + level.Height / 2 - camera.orthographicSize + clampoffset.y),
+                                            transform.position.z);
+        }
+        if (TrackRotation)
+        {
+            transform.rotation = target.rotation;
+        }
+	}
+}
---
+++ Legend/Assets/Scripts/Level.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour
+{
+    public Vector2 Center;
+    [Range(5, 100)]
+    public int Width = 5;
+    [Range(5, 100)]
+    public int Height = 5;
+    public GameObject border;
+    [SerializeField]
+    Transform levelparent;
+    [SerializeField]
+    GameObject background;
+    [SerializeField]
+    int bgtilesize;
+
+    void Start()
+    {
+        #region Border
+        Vector2 objectSize = border.GetComponent<BoxCollider2D>().size;
+        for (int row = 0; row < Height / objectSize.y; row++)
+        {
+            for (int col = 0; col < Width / objectSize.x; col++)
+            {
+                if (col == 0 || row == 0 ||
+                    col == (int)(Width / objectSize.x) - 1 || row == (int)(Height / objectSize.y) - 1)
+                {
+                    ((GameObject)Instantiate(border, new Vector3((col - Width / objectSize.x / 2 + .5f) * objectSize.x + Center.x,
+                                                                    (row - Height / objectSize.y / 2 + .5f) * objectSize.y + Center.y, 0), Quaternion.identity)).transform.SetParent(levelparent);
+                }
+            }
+        }
+        #endregion
+        #region BackGround
+        for (int row = 0; row < Height / bgtilesize; row++)
+        {
+            for (int col = 0; col < Width / bgtilesize - 1; col++)
+            {
+                ((GameObject)Instantiate(background, new Vector3((col - Width / bgtilesize / 2 + .5f) * bgtilesize + Center.x, (row - Height / bgtilesize / 2 + .5f) * bgtilesize + Center.y, 0), Quaternion.identity)).transform.SetParent(levelparent);
+            }
+        }
+        #endregion
+    }
+
+    void Update()
+    {
+
+    }
+}
