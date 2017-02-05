ecfae4e54ace74b664e71055d3949b57feb7b898
Ryan Alameddine <rhalameddine@gmail.com>
2017-02-05 10:47:20 -0800

    Began adding Terrain Generation

---
+++ Legend/Assets/Scripts/Noise/MapGenerator.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int seed;
+
+    public Transform playerTransform;
+
+    public Renderer textureRenderer;
+
+    public enum DrawMode { NoiseMap, ColorMap };
+    public DrawMode drawMode;
+
+    public const int mapChunkSize = 250;
+    public int noiseScale;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public bool CombineMap;
+    [HideInInspector]
+    public MapGenerator heatMap;
+    [HideInInspector]
+    public MapGenerator moistureMap;
+
+    void Start()
+    {
+        seed = Random.Range(0, int.MaxValue);
+        DrawMap();
+    }
+
+    public void DrawMap()
+    {
+        MapData mapData = GenerateMapData();
+
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            Texture2D texture = TextureGenerator.TextureFromHeightMap(mapData.heightMap);
+            textureRenderer.sharedMaterial.mainTexture = texture;
+            textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+        }
+        else if (drawMode == DrawMode.ColorMap)
+        {
+            Texture2D texture = TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize);
+            textureRenderer.sharedMaterial.mainTexture = texture;
+            textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+        }
+    }
+
+    MapData GenerateMapData()
+    {
+        float[,] noiseMap = Noise.GeterateNoiseMap(mapChunkSize, mapChunkSize, noiseScale, seed, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
+        {
+            for (int x = 0; x < mapChunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new MapData(noiseMap, colorMap);
+    }
+
+    private void OnValidate()
+    {
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            lacunarity = 0;
+        }
+    }
+
+    public void DrawTexture(Texture2D texture)
+    {
+
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+
+}
+
+
+public struct MapData
+{
+    public readonly float[,] heightMap;
+    public readonly Color[] colorMap;
+
+    public MapData(float[,] heightMap, Color[] colorMap)
+    {
+        this.heightMap = heightMap;
+        this.colorMap = colorMap;
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public float height;
+    public Color color;
+}
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.DrawMap();
+            }
+        }
+        if (!mapGen.autoUpdate)
+        {
+            if (GUILayout.Button("Generate"))
+            {
+                mapGen.DrawMap();
+            }
+        }
+        if (mapGen.CombineMap)
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.TextArea("")
+            mapGen.heatMap = (MapGenerator) EditorGUILayout.ObjectField(mapGen.heatMap, typeof(MapGenerator), true);
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Legend/Assets/Scripts/Noise/Noise.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise {
+    public static float[,] GeterateNoiseMap(int mapWidth, int mapHeight, float scale, int seed, int octaves, float persistance, float lacunarity, Vector2 offset)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        System.Random rand = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        for(int i = 0; i < octaves; i++)
+        {
+            float offsetX = rand.Next(-100000, 100000) + offset.x;
+            float offsetY = rand.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        if(scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHeight = mapHeight / 2f;
+
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float amplitutde = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+
+                    float sampleX = (x + octaveOffsets[i].x - halfWidth) / scale * frequency;
+                    float sampleY = (y + octaveOffsets[i].y - halfHeight) / scale * frequency;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+
+                    noiseHeight += perlinValue * amplitutde;
+
+                    amplitutde *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if(noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }else if(noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(-1.3f, .9f, noiseMap[x, y]);
+            }
+        }
+
+        return noiseMap;
+    }
+}
