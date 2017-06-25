9197f89d93c81afe8b270877a8bd4836fb6918ba
Ryan Alameddine <rhalameddine@gmail.com>
2017-06-25 10:46:11 -0700

    Added base for A* pathfinding

--- Legend/Assets/Scripts/AI/AStar.cs
+++ Legend/Assets/Scripts/AI/AStar.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AStar : BehaviorBase {
+public class AStar : BehaviorBase
+{
     public Vector2 start;
     public Vector2 target;
     bool active = false;
     bool stopped = true;
-    List<ATile> openTiles = new List<ATile>();
-    List<ATile> closedTiles = new List<ATile>();
+    TileList<ATile> openTiles;
+    TileList<ATile> closedTiles;
+    ATile targetTile;
+    public GameObject square;
+    bool solved = false;
 
     public override float run()
     {
@@ -18,22 +22,49 @@ public class AStar : BehaviorBase {
 
     private void Update()
     {
-        if(active && stopped)
+        if (active && stopped)
         {
+            solved = false;
             start = V2Int(transform.position);
-            target = start + new Vector2(5, 3);
+            target = start + new Vector2(5, 0);
+            openTiles = new TileList<ATile>(start, target);
+            closedTiles = new TileList<ATile>(start, target);
             stopped = false;
             closedTiles.Add(start);
             addAdjacent(start);
+            targetTile = target;
+            targetTile.generateScore(target, start);
         }
         if (active)
         {
-            
+            if (checkSolved() && !solved)
+            {
+                solved = true;
+                foreach(ATile solvedTile in closedTiles)
+                {
+                    Instantiate(square, solvedTile.position, Quaternion.identity);
+                }
+            }
+            if (!solved)
+            {
+                ATile lowest = findTile();
+                openTiles.Remove(closedTiles.Add(lowest));
+                addAdjacent(lowest.position);
+            }
         }
         else stopped = true;
 
         active = false;
-        
+
+    }
+
+    public bool checkSolved()
+    {
+        foreach(ATile solvedTile in closedTiles)
+        {
+            if (solvedTile.position == targetTile.position) return true;
+        }
+        return false;
     }
 
     public override bool check()
@@ -48,28 +79,98 @@ public class AStar : BehaviorBase {
 
     void addAdjacent(Vector2 middle)
     {
-        openTiles.Add(middle + new Vector2(0, 1));
-        openTiles.Add(middle + new Vector2(1, 0));
-        openTiles.Add(middle + new Vector2(-1, 0));
-        openTiles.Add(middle + new Vector2(0, -1));
+        if(!checkExists(middle + new Vector2(0, 1)))
+            openTiles.Add(middle + new Vector2(0, 1));
+        if (!checkExists(middle + new Vector2(1, 0)))
+            openTiles.Add(middle + new Vector2(1, 0));
+        if (!checkExists(middle + new Vector2(-1, 0)))
+            openTiles.Add(middle + new Vector2(-1, 0));
+        if (!checkExists(middle + new Vector2(0, -1)))
+            openTiles.Add(middle + new Vector2(0, -1));
     }
-    
+
+    bool checkExists(Vector2 position)
+    {
+        foreach (ATile open in openTiles)
+        {
+            if(open.position == position)
+            {
+                return true;
+            }
+        }
+
+        foreach (ATile closed in closedTiles)
+        {
+            if (closed.position == position)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     Vector2 V2Int(Vector2 convert)
     {
-        return new Vector2((int) convert.x, (int) convert.y);
+        return new Vector2((int)convert.x, (int)convert.y);
     }
 
-
+    public ATile findTile()
+    {
+        TileList<ATile> reversedTiles = openTiles;
+        reversedTiles.Reverse();
+        ATile lowestTile = reversedTiles.ToArray()[0];
+        foreach(ATile tile in reversedTiles)
+        {
+            if(tile.score <= lowestTile.score)
+            {
+                lowestTile = tile;
+            }
+        }
+        return lowestTile;
+    }
 }
 
-class ATile
+public class TileList<T> : List<T> where T : ATile
 {
-    Vector2 position;
-    int score;
+    Vector2 start;
+    Vector2 target;
+    public TileList(Vector2 start, Vector2 target) : base(){
+        this.start = start;
+        this.target = target;
+    }
+
+    public new ATile Add(T item)
+    {
+        
+        item.generateScore(target, start);
+        base.Add(item);
+        return item;
+    }
+}
+
+public class ATile
+{
+    public Vector2 position;
+    public int score;
 
     public ATile(Vector2 position, int score)
     {
         this.position = position;
         this.score = score;
-    } //make a creation mechanism from vectors
+    }
+
+    public void generateScore(Vector2 target, Vector2 start)
+    {
+        score = ManhattanDistance(start, position) + ManhattanDistance(target, position);
+    }
+
+    int ManhattanDistance(Vector2 start, Vector2 end)
+    {
+        return (int)Mathf.Abs(start.x - end.x) + (int)Mathf.Abs(start.y - end.y);
+    }
+
+    public static implicit operator ATile(Vector2 position)
+    {
+        return new ATile(position, 0);
+    }
 }
